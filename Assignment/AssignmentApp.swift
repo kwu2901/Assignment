71f6bc74346302d2ea4f1d5fc625bd79ece0a7e3
kwu2901 <kwu2901@gmail.com>
2023-01-08 21:59:31 +0800

    update tab bar & content view

--- Assignment/AssignmentApp.swift
+++ Assignment/AssignmentApp.swift
@@ -10,11 +10,17 @@ import SwiftUI
 @main
 struct AssignmentApp: App {
     let persistenceController = PersistenceController.shared
-
+    @StateObject var productManager = ProductManager()
     var body: some Scene {
         WindowGroup {
             ContentView()
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
+                .environmentObject(CameraModel())
+                .environmentObject(productManager)
+                .onAppear{
+                    UserDefaults.standard.setValue(false, forKey: "_UIConstraintBasedLayoutLogUnsatisfiable")
+                }
         }
     }
 }
+
