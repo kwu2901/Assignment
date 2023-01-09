3ecc5169a24475fe5c7a7e6f6035bc209bcde53b
kwu2901 <kwu2901@gmail.com>
2023-01-10 00:26:54 +0800

    add FileManager and ErrorController to Manager

---
+++ Assignment/Manager/ErrorController.swift
@@ -0,0 +1,44 @@
+//
+//  ErrorController.swift
+//  Assignment
+//
+//  Created by itst on 6/1/2023.
+//
+
+import SwiftUI
+
+enum ErrorController: Error, LocalizedError {
+    case readError
+    case decodingError
+    case encodingError
+    case saveError
+    case saveImageError
+    case readImageError
+    
+    var errorDescription: String? {
+        switch self {
+        case .readError:
+            return NSLocalizedString("Could not load MyImage.json, please reinstall the app.", comment: "")
+        case .decodingError:
+            return NSLocalizedString("There was a problem loading your list of images, please create a new image to start over.", comment: "")
+        case .encodingError:
+            return NSLocalizedString("Could not save your MyImage data, please reinstall the app.", comment: "")
+        case .saveError:
+            return NSLocalizedString("Could not save the My Image json file.  Please reinstall the app.", comment: "")
+        case .saveImageError:
+            return NSLocalizedString("Could not save image.  Please reinstall the app.", comment: "")
+        case .readImageError:
+            return NSLocalizedString("Could not load image.  Please reinstall the app.", comment: "")
+        }
+    }
+    
+    struct ErrorType: Identifiable {
+        let id = UUID()
+        let error: ErrorController
+        var message: String {
+            error.localizedDescription
+        }
+        let button = Button("OK", role: .cancel) {}
+    }
+}
+
---
+++ Assignment/Manager/FileManager.swift
@@ -0,0 +1,66 @@
+//
+//  FileManager.swift
+//  Assignment
+//
+//  Created by itst on 6/1/2023.
+//
+
+import UIKit
+
+let fileName = "MyProducts.json"
+
+extension FileManager {
+    static var docDirURL: URL {
+        return Self.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+    }
+    
+    func docExist(named docName: String) -> Bool {
+        fileExists(atPath: Self.docDirURL.appendingPathComponent(docName).path)
+    }
+    
+    func saveDocument(contents: String) throws {
+        let url = Self.docDirURL.appendingPathComponent(fileName)
+        do {
+            try contents.write(to: url, atomically: true, encoding: .utf8)
+        } catch {
+            throw ErrorController.saveError
+        }
+    }
+    
+    func readDocument() throws -> Data {
+        let url = Self.docDirURL.appendingPathComponent(fileName)
+        do {
+            return try Data(contentsOf: url)
+        } catch {
+            throw ErrorController.readError
+        }
+    }
+    
+    func saveImage(_ id: String, image: UIImage) throws {
+        if let data = image.jpegData(compressionQuality: 0.6) {
+            let imageURL = FileManager.docDirURL.appendingPathComponent("\(id).jpg")
+            do {
+                try data.write(to: imageURL)
+            } catch {
+                throw ErrorController.saveImageError
+            }
+        } else {
+            throw ErrorController.saveImageError
+        }
+    }
+    
+    func readImage(with id: UUID) throws -> UIImage {
+        let imageURL = FileManager.docDirURL.appendingPathComponent("\(id).jpg")
+        do {
+            let imageData = try Data(contentsOf: imageURL)
+            if let image = UIImage(data: imageData) {
+                return image
+            } else {
+                throw ErrorController.readImageError
+            }
+        } catch {
+            throw ErrorController.readImageError
+        }
+    }
+}
+
