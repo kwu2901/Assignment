a0e40e044083af69dd7381e9dabd7993ad258a32
kwu2901 <kwu2901@gmail.com>
2023-01-10 23:33:58 +0800

    add payment button & payment handler

---
+++ Assignment/Components/PaymentButton.swift
@@ -0,0 +1,63 @@
+//
+//  PaymentButton.swift
+//  Assignment
+//
+//  Created by itst on 6/1/2023.
+//
+
+import SwiftUI
+import PassKit
+
+struct PaymentButton: View {
+    var action: () -> Void
+    
+    var body: some View {
+        Representatble(action: action)
+            .frame(minWidth: 100, maxWidth: 400)
+            .frame(height: 45)
+            .frame(maxWidth: .infinity)
+    }
+}
+
+extension PaymentButton {
+    struct Representatble: UIViewRepresentable {
+        var action: () -> Void
+        
+        func makeCoordinator() -> Coordinator {
+            Coordinator(action: action)
+        }
+        
+        func makeUIView(context: Context) -> some UIView {
+            context.coordinator.button
+        }
+        
+        func updateUIView(_ uiView: UIViewType, context: Context) {
+            context.coordinator.action = action
+        }
+    }
+    
+    class Coordinator: NSObject {
+        var action: () -> Void
+        var button = PKPaymentButton(paymentButtonType: .checkout, paymentButtonStyle: .automatic)
+        
+        init(action: @escaping () -> Void) {
+            self.action = action
+            super.init()
+            
+            button.addTarget(self, action: #selector(callback(_:)), for: .touchUpInside)
+        }
+        
+        @objc
+        func callback(_ sender:Any) {
+            action()
+        }
+    }
+}
+
+struct PaymentButton_Previews: PreviewProvider {
+    static var previews: some View {
+        PaymentButton(action: {})
+    }
+}
+
+
