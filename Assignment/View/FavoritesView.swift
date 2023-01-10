f9d6eb29b626307c4a448efb5d5034b8f20a35b9
kwu2901 <kwu2901@gmail.com>
2023-01-10 23:36:29 +0800

    add favorites view and setting view

---
+++ Assignment/View/FavoritesView.swift
@@ -0,0 +1,20 @@
+//
+//  FavoritesView.swift
+//  Assignment
+//
+//  Created by itst on 6/1/2023.
+//
+
+import SwiftUI
+
+struct FavoritesView: View {
+    var body: some View {
+        Text("FavoritesView")
+    }
+}
+
+struct FavoritesView_Previews: PreviewProvider {
+    static var previews: some View {
+        FavoritesView()
+    }
+}
---
+++ Assignment/View/SettingView.swift
@@ -0,0 +1,53 @@
+//
+//  SettingView.swift
+//  Assignment
+//
+//  Created by itst on 6/1/2023.
+//
+
+import SwiftUI
+
+struct SettingView: View {
+    @StateObject var model = BiometricModel()
+    @State private var loginSucc = false
+    @Binding var root: String
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .foregroundColor(.accentColor)
+                if model.isError == true {
+                    Text("\(model.errorMessage)")
+                }
+                Button(action: {
+                    model.evaluatePolicy()
+                    loginSucc = model.isAuthenicated
+                    if loginSucc == true {
+                        root = "Admin"
+                    }
+                }, label: {
+                    if loginSucc == true {
+                        Text(root)
+                    }else {
+                        Text("Click to Login")
+                    }
+                })
+                
+            }
+            .onAppear {
+                model.checkPolicy()
+            }
+            .padding()
+
+        }
+    }
+}
+
+struct SettingView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingView(root: .constant("Client"))
+    }
+}
