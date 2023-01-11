366be0cb9fa120e48574df36c2880fe586bb55c9
kwu2901 <kwu2901@gmail.com>
2023-01-11 23:11:44 +0800

    add environmentObject in App.swift

--- Assignment/AssignmentApp.swift
+++ Assignment/AssignmentApp.swift
@@ -9,13 +9,12 @@ import SwiftUI
 
 @main
 struct AssignmentApp: App {
-    let persistenceController = PersistenceController.shared
     @StateObject var productManager = ProductManager()
     var body: some Scene {
         WindowGroup {
             ContentView()
-                .environment(\.managedObjectContext, persistenceController.container.viewContext)
                 .environmentObject(CameraModel())
+                .environmentObject(CartManager())
                 .environmentObject(productManager)
                 .onAppear{
                     UserDefaults.standard.setValue(false, forKey: "_UIConstraintBasedLayoutLogUnsatisfiable")
